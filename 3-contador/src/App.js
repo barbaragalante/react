af0955a263681189bec377b545dfc8795eb08165
Barbara <contato.barbaragalante@gmail.com>
2019-11-12 15:55:21 -0300

    exercicio contador

---
+++ 3-contador/src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './App.css';   
+
+class Contador extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contador: 0
+    };
+  }
+
+  adicionarUm = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador + 1
+      }
+    })
+  }
+
+  subtraiUm = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador -1
+      }
+    })
+  }
+
+  resetar = () => {
+    this.setState((prevState) => {
+      return {
+        contador: prevState.contador = 0
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      // {this.state.visivel === true ? '' : }
+      <div>
+        <p>{this.state.contador}</p>
+        <button className="btnMaisUm" onClick={this.adicionarUm}>+1</button>
+        <button className="btnMenosUm" onClick={this.subtraiUm}>-1</button>
+        <button className="btnResetar" onClick={this.resetar}>resetar</button>
+      </div>
+    );
+  }
+}
+
+function App(props) {
+  return (
+    <div className="App">
+      <h2>Contador</h2>
+      <Contador />
+    </div>
+  );
+}
+export default App; 
